2579187361ca1f73135c434034b5cdcdf2a25189
viionc <marek.d.stankiewicz@gmail.com>
2023-09-29 18:41:21 +0200

    fixed form

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -17,14 +17,31 @@ function ContactPage() {
                 >
                     <div className="w-full">
                         <label
+                            htmlFor="name"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        >
+                            Your name
+                        </label>
+                        <input
+                            type="text"
+                            name="contact-name"
+                            id="contact-name"
+                            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                            placeholder="John Doe"
+                            required
+                        />
+                    </div>
+                    <div className="w-full">
+                        <label
                             htmlFor="email"
                             className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                         >
                             Your email
                         </label>
                         <input
-                            type="email"
+                            type="text"
                             id="email"
+                            name="email"
                             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                             placeholder="name@flowbite.com"
                             required
@@ -40,6 +57,7 @@ function ContactPage() {
                         <input
                             type="text"
                             id="subject"
+                            name="subject"
                             className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                             placeholder="Let me know what you thought about my portfolio!"
                             required
@@ -54,6 +72,7 @@ function ContactPage() {
                         </label>
                         <textarea
                             id="message"
+                            name="message"
                             rows={6}
                             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                             placeholder="Leave a message..."
