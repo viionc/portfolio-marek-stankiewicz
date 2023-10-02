7be6a4a56099cc99708705c0682c27039b89e72b
viionc <marek.d.stankiewicz@gmail.com>
2023-10-02 17:48:57 +0200

    added logos

--- src/components/SkillsPage.tsx
+++ src/components/SkillsPage.tsx
@@ -1,13 +1,10 @@
 import Skill from "./Skill";
 
 function SkillsPage() {
-    const skills = ["html", "css", "javascript", "typescript", "react", "bootstrap", "git"];
+    const skills = ["html", "css", "javascript", "typescript", "react", "bootstrap", "git", "tailwind", "nodejs"];
 
     return (
-        <section
-            id="skills-page"
-            className="flex mt-36 gap-5 flex-wrap justify-center items-center scroll-mt-32 z-10"
-        >
+        <section id="skills-page" className="flex mt-36 gap-5 flex-wrap justify-center items-center scroll-mt-32 z-10">
             {skills.map((skill, index) => (
                 <Skill key={index} skill={skill} delay={0.1 * index}></Skill>
             ))}
